3640d4c90410a4652d8ea5fc539d930f5c76fd6b
apoorvam07 <apoorvakshtriya18@gmail.com>
2023-11-21 13:00:52 +0530

    From alert ,datatypes,loops,functions

---
+++ Java script/Practice/Practice.js
@@ -0,0 +1,14 @@
+//Alert and Prompts
+
+// food = Number(prompt("how much for food"));
+// tipPercentage = Number(prompt("how much tip for the food") / 100);
+// tip = food * tipPercentage;
+// alert(tip);
+// alert(tip + food);
+//console.log(tip);
+
+// functions with return
+function sum(a, b) {
+  return a + b;
+}
+sum(1, 3);
